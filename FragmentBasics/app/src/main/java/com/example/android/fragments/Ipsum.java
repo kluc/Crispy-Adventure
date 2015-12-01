19bb283144476074cac5436a5d7b56f4e27f9723
Kelson Luc <Kelson@chandraanthsmbp.na.xerox.net>
2015-12-01 12:22:49 -0800

    commiting right project this time

---
+++ FragmentBasics/app/src/main/java/com/example/android/fragments/Ipsum.java
@@ -0,0 +1,29 @@
+/*
+ * Copyright (C) 2012 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.example.android.fragments;
+
+public class Ipsum {
+
+    static String[] Headlines = {
+        "Article One",
+        "Article Two"
+    };
+
+    static String[] Articles = {
+            "New Article one",
+            "Article Two\n\nVinyl williamsburg non velit, master cleanse four loko banh mi. Enim kogi keytar trust fund pop-up portland gentrify. Non ea typewriter dolore deserunt Austin. Ad magna ethical kogi mixtape next level. Aliqua pork belly thundercats, ut pop-up tattooed dreamcatcher kogi accusamus photo booth irony portland. Semiotics brunch ut locavore irure, enim etsy laborum stumptown carles gentrify post-ironic cray. Butcher 3 wolf moon blog synth, vegan carles odd future."
+    };
+}
---
+++ FragmentBasics/app/src/main/java/com/example/android/fragments/TestFragment.java
@@ -0,0 +1,14 @@
+package com.example.android.fragments;
+
+/**
+ * Created by Kelson on 12/1/15.
+ */
+public class TestFragment {
+
+    public void TestMethod(){
+        //Should not do anything
+
+        int x = 0;
+        x = x + 10;
+    }
+}
